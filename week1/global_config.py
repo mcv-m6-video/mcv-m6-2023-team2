b29b870f98bb67c16e02cacb629f81a30774f99c
AtenrevCode <atenrev@gmail.com>
2023-03-13 21:51:58 +0100

    Visualize frames

--- week1/global_config.py
+++ week1/global_config.py
@@ -2,4 +2,5 @@ DATASET_PATH = "data/AICity_data/train/S03/c010"
 
 # TASK 1
 ANNOTATIONS_PATH = f"{DATASET_PATH}/ai_challenge_s03_c010-full_annotation.xml"
-PREDICTIONS_PATH = f"{DATASET_PATH}/det/det_mask_rcnn.txt"
+PREDICTIONS_PATH = f"{DATASET_PATH}/det/det_yolo3.txt"
+VIDEO_PATH = f"{DATASET_PATH}/vdo.avi"
