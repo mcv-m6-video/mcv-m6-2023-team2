20b45a05af9cb27849bb821b254c85e8936a2710
Alex <21alexth@gmail.com>
2023-04-10 21:55:46 +0200

    tracking with OF pipeline

--- week4/task_1_3.py
+++ week4/task_1_3.py
@@ -1,11 +1,11 @@
 from __future__ import print_function
 
-import sys
 import os
 import argparse
 import numpy as np
 import time
 import cv2
+from typing import List
 
 from tqdm import tqdm
 
@@ -13,57 +13,105 @@ from utils_w4 import (
     load_predictions,
     group_annotations_by_frame,
     filter_annotations,
-    non_maxima_suppression
+    non_maxima_suppression,
+    load_optical_flow,
+    read_flow_unimatch,
 )
-from tracking.tracking_utils import TrackingViz
+from tracking.tracking_utils import TrackHandlerOverlap, TrackingViz, Track
 from class_utils import BoundingBox
-# from tracking.sort import Sort
-from tracking.sort_of import Sort
-
-from of.optical_flow import BlockMatching
 
 
 def __parse_args() -> argparse.Namespace:
     parser = argparse.ArgumentParser(
-        description='Road Traffic Monitoring Analysis for Video Surveillance. MCV-M6-Project, week 3, task 2.2. Team 2'
+        description='Road Traffic Monitoring Analysis for Video Surveillance. MCV-M6-Project, week 4, task 1.3. Team 2'
     )
 
     parser.add_argument('--path_sequence', type=str, default="data/AICity_data/train/S03/c010/vdo.avi",
                         help='Path to the directory where the sequence is stored.')
 
-    parser.add_argument('--path_of_data', type=str, default="./week4/results/video_of/",
-                        help='Path to the directory where the sequence is stored.')
+    parser.add_argument('--tracking_type', type=str, default="kalman",
+                        help='Type of tracking to be used. Options: "overlap", "kalman".')
+
+    parser.add_argument('--model', type=str, default="faster_finetune",
+                        help='Path to the directory where the detections are stored.')
+
+    parser.add_argument('--of_use', action='store_true', default=True,
+                        help='Whether to use optical flow.')
+
+    parser.add_argument('--of_type', type=str, default="unimatch",
+                        help='Type of optical flow to be used. Options: "block_match", "unimatch".')
+
+    parser.add_argument('--path_of_data', type=str, default="./week4/results/video_of_unimatch/",
+                        help='Path to the directory where the optical flow is stored.')
 
     parser.add_argument('--path_results', type=str, default="./week4/results/",
-                    help='The path to the directory where the results will be stored.')
+                        help='The path to the directory where the results will be stored.')
 
     parser.add_argument('--path_tracking_data', type=str, default="./week4/data/trackers/mot_challenge/parabellum-train",
-                    help='The path to the directory where the results will be stored.')
+                        help='The path to the directory where the results will be stored.')
     
     args = parser.parse_args()
     return args
 
 
-def readFlow(fn):
-    """ Read .flo file in Middlebury format"""
-    # Code adapted from:
-    # http://stackoverflow.com/questions/28013200/reading-middlebury-flow-files-with-python-bytes-array-numpy
+def save_track_results(tracks: List[Track], results_file):
+    for t in tracks:
+        detections = t.detections
+        for d in detections:
+            results_file.write(
+                f"{d.frame + 1},{d.track_id},{d.x1},{d.y1},{d.x2 - d.x1},{d.y2 - d.y1},{d.confidence if d.confidence else '-1'},-1,-1,-1\n")
 
-    # WARNING: this will work on little-endian architectures (eg Intel x86) only!
-    # print 'fn = %s'%(fn)
-    with open(fn, 'rb') as f:
-        magic = np.fromfile(f, np.float32, count=1)
-        if 202021.25 != magic:
-            print('Magic number incorrect. Invalid .flo file')
-            return None
-        else:
-            w = np.fromfile(f, np.int32, count=1)
-            h = np.fromfile(f, np.int32, count=1)
-            # print 'Reading %d x %d flo file\n' % (w, h)
-            data = np.fromfile(f, np.float32, count=2 * int(w) * int(h))
-            # Reshape testdata into 3D array (columns, rows, bands)
-            # The reshape here is for visualization, the original code is (w,h,2)
-            return np.resize(data, (int(h), int(w), 2))
+
+def tracking_by_maximum_overlap(
+        video_path: str,
+        detections: List[BoundingBox],
+        model_name: str,
+        save_path: str,
+        save_tracking_path: str,
+        max_frame_skip: int = 0,
+        min_iou: float = 0.5,
+):
+    track_handler = TrackHandlerOverlap(max_frame_skip=max_frame_skip, min_iou=min_iou)
+    pass
+
+    video = cv2.VideoCapture(video_path)
+    total_frames = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
+    width = int(video.get(cv2.CAP_PROP_FRAME_WIDTH))
+    height = int(video.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
+    output_video_path = os.path.join(save_path, f"{model_name}.mp4")
+    tracking_viz = TrackingViz(output_video_path, width, height, fps=10)
+
+    for frame_id in tqdm(range(total_frames)):
+        ret, frame = video.read()
+        if not ret:
+            break
+
+        frame_detections = detections[frame_id]
+        track_handler.update_tracks(frame_detections, frame_id)
+
+        # Visualize tracking
+        viz_detections = []
+        for track in track_handler.live_tracks:
+            detection, _ = track.last_detection()
+            viz_detections.append(detection)
+
+        tracking_viz.draw_tracks(frame, viz_detections)
+        tracking_viz.draw_trajectories(frame)
+        tracking_viz.write_frame(frame)
+
+    # Save tracking results
+    save_tracking_path = os.path.join(save_tracking_path, model_name, "data")
+    os.makedirs(save_tracking_path, exist_ok=True)
+    results_file = open(os.path.join(save_tracking_path, "s03.txt"), "w")
+    save_track_results(track_handler.terminated_tracks, results_file)
+    save_track_results(track_handler.live_tracks, results_file)
+
+    total_terminated_tracks = len(track_handler.terminated_tracks)
+    total_live_tracks = len(track_handler.live_tracks)
+    print(f"Total terminated tracks: {total_terminated_tracks}")
+    print(f"Total live tracks: {total_live_tracks}")
+    print(f"Total tracks: {total_terminated_tracks + total_live_tracks}")
 
 
 def tracking_by_kalman_filter_with_optical_flow(
@@ -76,12 +124,9 @@ def tracking_by_kalman_filter_with_optical_flow(
         tracking_max_age: int = 1,
         tracking_min_hits: int = 3,
         tracking_iou_threshold: float = 0.3,
-        of_data_path: str = None,
-        of_block_size: int = 16,
-        of_search_window_size: int = 64,
-        of_estimation_type: str = "mean",
-        of_error_function: str = "mse",
-        of_color_space: str = "gray",
+        of_use: bool = False,
+        of_data_path: str = None,   # Path to the optical flow data. Only used if of_use is True.
+        of_type: str = "block_match",  # Type of optical flow. Options: "block_match", "unimatch"
 ):
     # Reference: https://github.com/telecombcn-dl/2017-persontyle/blob/master/sessions/tracking/tracking_kalman.ipynb
     save_tracking_path = os.path.join(save_tracking_path, model_name, "data")
@@ -105,13 +150,16 @@ def tracking_by_kalman_filter_with_optical_flow(
     tracking_viz = TrackingViz(output_video_path, video_width, video_height, fps)
     results_file = open(os.path.join(save_tracking_path, "s03.txt"), "w")
 
+    if of_use:
+        from tracking.sort_of import Sort
+        print("Imported SORT with Optical Flow.")
+    else:
+        from tracking.sort import Sort
+        print("Imported SORT.")
+
     mot_tracker = Sort(max_age=tracking_max_age, min_hits=tracking_min_hits, iou_threshold=tracking_iou_threshold)
 
-    # block_matching = BlockMatching(block_size=of_block_size, search_window_size=of_search_window_size,
-    #                                estimation_type=of_estimation_type, error_function=of_error_function)
-
-    for idx_frame in tqdm(range(0, max_frames, video_frame_sampling), desc="Computing Kalman filter Tracking with "
-                                                                             "Optical Flow..."):
+    for idx_frame in tqdm(range(0, max_frames-1, video_frame_sampling), desc="Computing tracking..."):
         # Read detections
         if len(detections) <= idx_frame:
             dets = []
@@ -119,7 +167,7 @@ def tracking_by_kalman_filter_with_optical_flow(
             dets = detections[idx_frame]   
 
         # read the frame
-        video.set(cv2.CAP_PROP_POS_FRAMES, idx_frame)
+        # video.set(cv2.CAP_PROP_POS_FRAMES, idx_frame)
         ret, frame = video.read()
 
         if not ret:
@@ -136,27 +184,23 @@ def tracking_by_kalman_filter_with_optical_flow(
 
         start_time = time.time()
 
-        # if of_color_space == "gray":
-        #     pred_flow = block_matching.estimate_optical_flow(
-        #         cv2.cvtColor(frame_prev, cv2.COLOR_BGR2GRAY),
-        #         cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY),
-        #         leave_tqdm=False
-        #     )
-        # else:
-        #     pred_flow = block_matching.estimate_optical_flow(
-        #         frame_prev,
-        #         frame,
-        #         leave_tqdm=False
-        #     )
-
         # Read optical flow file of the current frame (saved in disk)
-        if of_data_path is not None:
-            pred_flow = readFlow(os.path.join(of_data_path, f"{idx_frame:04d}_pred.flo"))
-        else:
-            pred_flow = np.zeros((video_width, video_height, 2))
+        if of_use and of_data_path is not None:
+            if of_type == "block_match":
+                pred_flow = load_optical_flow(os.path.join(of_data_path, f"{idx_frame}.png"))
+            elif of_type == "unimatch":
+                pred_flow = read_flow_unimatch(os.path.join(of_data_path, f"{idx_frame:04d}_pred.flo"))
+                # pred_flow = load_optical_flow(os.path.join(of_data_path, f"{idx_frame:04d}_flow.png"))
+                print(pred_flow.shape)
+            else:
+                raise ValueError(f"Invalid optical flow type: {of_type}. Options: 'block_matching', 'unimatch'")
 
-        trackers = mot_tracker.update(dets, pred_flow)
-        # trackers = mot_tracker.update(dets)
+            # Update tracker
+            trackers = mot_tracker.update(dets, pred_flow)
+        else:
+            # Update tracker
+            trackers = mot_tracker.update(dets)
+
         cycle_time = time.time() - start_time
         total_time += cycle_time
         
@@ -179,46 +223,65 @@ def tracking_by_kalman_filter_with_optical_flow(
 def main(args: argparse.Namespace):
 
     # Path will be like this: ./week3/data/trackers/mot_challenge/parabellum-train/MODEL_NAME/data/s03.txt
-    model_name = "retina_of"
-    detections_path = f"week4/results/{model_name}/detections.txt"
-    confidence_threshold = 0.5
-    # Tracking parameters
-    max_age = 50
-    min_hits = 3
-    min_iou = 0.3
-    # Optical Flow parameters
-    block_size = 24
-    search_window_size = 76
-    estimation_type = "forward"
-    error_function = "nccorr"
-    color_space = "gray"
+    detections_path = f"week4/results/{args.model}/detections.txt"
 
+    # Load and process detections
+    confidence_threshold = 0.5
     detections = load_predictions(detections_path)
     detections = filter_annotations(detections, confidence_thr=confidence_threshold)
     detections = group_annotations_by_frame(detections)
     detections = non_maxima_suppression(detections)
-    # model_name_for_file = f"kalman_{model_name.replace('_', '-')}_block_{block_size}_window" \
-    #                       f"_{search_window_size}_type_{estimation_type}_error_{error_function}_color_{color_space}"
 
-    model_name_for_file = f"kalman_{model_name.replace('_', '-')}_unimatch" \
+    model_name_for_file = f"{args.tracking_type}_{args.model.replace('_', '-')}_OF_" \
+                          f"{args.of_type if args.of_use else 'none'}"
 
-    # FIXME: Not optimal (at least with BlockMatching). ETA: +23 hours
-    tracking_by_kalman_filter_with_optical_flow(
-        detections=detections,
-        model_name=model_name_for_file,
-        save_video_path=args.path_results,
-        save_tracking_path=args.path_tracking_data,
-        # video_max_frames=45,
-        tracking_max_age=max_age,
-        tracking_min_hits=min_hits,
-        tracking_iou_threshold=min_iou,
-        of_data_path=args.path_of_data,
-        of_block_size=block_size,
-        of_search_window_size=search_window_size,
-        of_estimation_type=estimation_type,
-        of_error_function=error_function,
-        of_color_space=color_space,
-    )
+    if args.tracking_type == "kalman":
+
+        # BEST Kalman filter Tracking parameters (found in WK 3)
+        max_age = 50
+        min_hits = 3
+        min_iou = 0.3
+
+        tracking_by_kalman_filter_with_optical_flow(
+            detections=detections,
+            model_name=model_name_for_file,
+            save_video_path=args.path_results,
+            save_tracking_path=args.path_tracking_data,
+            video_max_frames=45,    # TODO: Comment this line to use all frames!
+            tracking_max_age=max_age,
+            tracking_min_hits=min_hits,
+            tracking_iou_threshold=min_iou,
+            of_use=args.of_use,
+            of_data_path=args.path_of_data,
+            of_type=args.of_type,
+        )
+
+    elif args.tracking_type == "overlap":
+
+        if args.of_use:
+            # TODO: Implement this in "tracking > overlap_of.py"
+            raise ValueError("Overlap tracking does not support optical flow.")
+
+        else:
+            # BEST Overlap Tracking parameters (found in WK 3)
+            max_frame_skip = 10
+            min_iou = 0.5
+
+            tracking_by_maximum_overlap(
+                video_path=args.path_sequence,
+                detections=detections,
+                model_name=model_name_for_file,
+                save_path=args.path_results,
+                save_tracking_path=args.path_tracking_data,
+                max_frame_skip=max_frame_skip,
+                min_iou=min_iou,
+            )
+
+    else:
+        raise ValueError(f"Unknown tracking type: {args.tracking_type}. Valid values: 'kalman', 'overlap'.")
+
+    print(f"Tracking results saved in: {os.path.join(args.path_tracking_data, model_name_for_file)}")
+    print(f"Tracking video saved in: {os.path.join(args.path_results, f'task_1_3_{model_name_for_file}.mp4')}")
 
 
 if __name__ == "__main__":
