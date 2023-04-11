62b274784eddabdfb984529be1ee4806bec4ab04
EauDeData <amolina@cvc.uab.cat>
2023-04-11 19:33:54 +0200

    uwu

--- week4/visutils.py
+++ week4/visutils.py
@@ -181,7 +181,8 @@ def plot_optical_flow_surface(path,  original_image_path = None):
 
 def record_3d_video(seq_video_path, results_path, vidout = 'outpy.avi'):
 
-    results = sorted([results_path + r for r in os.listdir(results_path) if 'png' in r],)
+    files = [r for r in os.listdir(results_path) if 'png' in r and len(r.split('.')[0].split('/')[-1])]
+    results = sorted([results_path + r for r in files], key = lambda x:  int(x.split('.')[0].split('/')[-1]))
     video_handler = cv2.VideoCapture(seq_video_path)
 
     W = 480
@@ -221,7 +222,8 @@ def plot_optical_flow_quiver_with_centroids(path, original_image_path = None, ce
 
 def video_optical_quiver(seq_video_path, results_path, detections = None, vidout = 'outquiver.avi'):
 
-    results = sorted([results_path + r for r in os.listdir(results_path) if 'png' in r],)
+    files = [r for r in os.listdir(results_path) if 'png' in r and len(r.split('.')[0].split('/')[-1])]
+    results = sorted([results_path + r for r in files], key = lambda x:  int(x.split('.')[0].split('/')[-1]))
     if isinstance(detections, str):
         detections = open(detections, 'r').readlines()
         detections_lut = {}
