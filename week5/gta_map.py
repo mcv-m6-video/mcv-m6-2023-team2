d6670e7cdc84be867c9b2b7eef7bb2e2bc044442
AtenrevCode <atenrev@gmail.com>
2023-04-26 01:07:03 +0200

    timestamps

--- week5/gta_map.py
+++ week5/gta_map.py
@@ -18,6 +18,8 @@ def __parse_args() -> argparse.Namespace:
 
     parser.add_argument('--sequence_path', type=str, default='./data/aic19/train/S03',
                         help='Path to the sequence')
+    parser.add_argument('--timestamps_path', type=str, default='./data/aic19/cam_timestamp/S03.txt',
+                        help='Path to the timestamps file')
 
     args = parser.parse_args()
     return args
@@ -74,6 +76,14 @@ def main(args):
     cameras.sort()
     print('Cameras:', cameras)
 
+    # Load timestamps
+    start_timestamps = {}
+    with open(args.timestamps_path, 'r') as f:
+        for line in f:
+            camera, timestamp = line.split()
+            fps = 10 if camera != 'c015' else 8
+            start_timestamps[camera] = float(timestamp) * fps
+
     print("Mapping predictions to GPS coordinates...")
     predictions_in_gps = {}
     for camera in cameras:
@@ -85,12 +95,14 @@ def main(args):
         with open(homography_file, 'r') as f:
             homography_line = f.readline()
             homography = np.array([val.split() for val in homography_line.split(';')]).astype(np.float32)
-            distorion_coeffs = f.readline()
-            if distorion_coeffs:
-                continue
-                
+            # distorion_coeffs = f.readline()
+            # if distorion_coeffs:
+            #     continue
+
+        # Invert homography                
         homography = LA.inv(homography)
 
+        # Load predictions
         predictions = load_predictions(os.path.join(args.sequence_path, camera, 'gt/gt.txt'))
         predictions = group_annotations_by_frame(predictions)
 
@@ -109,7 +121,6 @@ def main(args):
     # print("Filtering points...")
     # filter_points(predictions_in_gps)
 
-
     # Generate 100 random colors
     colors = np.random.randint(0, 255, (100, 3), dtype=np.uint8)
 
@@ -126,11 +137,6 @@ def main(args):
                 max_x = max(max_x, prediction[0])
                 max_y = max(max_y, prediction[1])
 
-    # map_size = (int(np.ceil(max_y - min_y)), int(np.ceil(max_x - min_x)), 3)
-    # print(f"Map size: {map_size}")
-
-    # camera_map = np.zeros((map_size[0], map_size[1], 3), dtype=np.uint8)
-
     # Draw grayish background for all predictions
     camera_colors = np.random.randint(0, 255, (len(cameras), 3), dtype=np.uint8)
     for camera in cameras:
@@ -138,7 +144,6 @@ def main(args):
             for prediction in frame_predictions:
                 x, y = int(np.ceil((prediction[0] - min_x) / (max_x - min_x) * camera_map.shape[1])), \
                           int(np.ceil((prediction[1] - min_y) / (max_y - min_y) * camera_map.shape[0]))  
-                # x, y = int(np.ceil((prediction[0] - min_x))), int(np.ceil((prediction[1] - min_y)))
                 color = camera_colors[cameras.index(camera)]
                 color = (int(color[0] * 0.5), int(color[1] * 0.5), int(color[2] * 0.5))
                 cv2.circle(camera_map, (x, y), 24, color, -1)
@@ -163,7 +168,6 @@ def main(args):
 
     # Draw predictions in a video
     video = cv2.VideoWriter('map.avi', cv2.VideoWriter_fourcc(*'XVID'), 10, camera_map.shape[:2][::-1])
-    # video = cv2.VideoWriter('map.avi', cv2.VideoWriter_fourcc(*'XVID'), 10, (1920, 1080))
 
     print("Generating video...")
 
@@ -175,7 +179,7 @@ def main(args):
 
         # Draw predictions as circles
         for camera in cameras:
-            if idx_frame >= len(predictions_in_gps[camera]):
+            if idx_frame >= len(predictions_in_gps[camera]) or start_timestamps[camera] > idx_frame:
                 continue
 
             for prediction in predictions_in_gps[camera][idx_frame]:
@@ -184,7 +188,6 @@ def main(args):
                 # Map GPS coordinates so that they fit in the camera map image
                 x, y = int(np.ceil((prediction[0] - min_x) / (max_x - min_x) * camera_map.shape[1])), \
                           int(np.ceil((prediction[1] - min_y) / (max_y - min_y) * camera_map.shape[0])) 
-                # x, y = int(np.ceil((prediction[0] - min_x))), int(np.ceil((prediction[1] - min_y)))
                 cv2.circle(map_gps, (x, y), 8, color, -1)
                 # Write the track ID with a white background
                 cv2.putText(map_gps, str(prediction[2]), (x, y), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 2, cv2.LINE_AA)
