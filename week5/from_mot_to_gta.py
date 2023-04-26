fe9d920479b4d0f05ba991fd97a5cd50c04742a4
AtenrevCode <atenrev@gmail.com>
2023-04-27 00:33:23 +0200

    xd

--- week5/from_mot_to_gta.py
+++ week5/from_mot_to_gta.py
@@ -7,7 +7,7 @@ MOT_DIR = "./data/trackers/mot_challenge/parabellum-train/overlap_filtAreaTrue_f
 TARGET_DIR = "tracking_results/overlap_filtAreaTrue_filtParkedTrue/"
 
 for seq_cam in os.listdir(MOT_DIR):
-    seq, cam = seq_cam.split("_")
+    seq, cam = seq_cam.split(".")[0].split("_")
     seq_dir = os.path.join(TARGET_DIR, seq)
     cam_dir = os.path.join(seq_dir, cam)
     os.makedirs(cam_dir, exist_ok=True)
